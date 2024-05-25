a3678730eba383d078de16273e11d6214b2528fe
Abhishek <abhishek.112031@gmail.com>
2024-05-25 09:11:03 +0530

    time and search functionality

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,18 +1,31 @@
+import { useAuthContext } from "../../context/AuthContext";
+import { extractTime } from "../../utils/extractTime";
+import useConversation from "../../zustand/useConversation";
 
 
 const Message = (props) => {
+    const {authUser}=useAuthContext();
+    const {selectedConversation}=useConversation();
+    const fromMe=props?.msg.senderId==authUser?._id
+   const formattedTime=extractTime(props?.msg?.createdAt);
+
+    const chatClassName=fromMe ?  'chat-end': 'chat-start';
+    const profilePic=fromMe ? authUser.profilePic : selectedConversation.profilePic;
+    const bubbleBgColor= fromMe ? 'bg-blue-500' : " bg-dark-500";
+
+
 	return (
-		<div className="chat chat-end">
+		<div className={`chat ${chatClassName}`}>
            <div className="chat-image avatar">
             <div className="w-5 rounded-full">
-                <img src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ621JlOQ1RCRVoEoJ1sJftRtbn7NFk9Z8gtg&usqp=CAU"} 
+                <img src={profilePic} 
                 alt="" />
 
             </div>
 
            </div>
-           <div className={'chat-bubble text-white bg-blue-500 '}>{props.msg}</div>
-           <div className={'chat-footer opacity-50 text-xs flex gap-1 items-center text-slate-950'}> 12:42</div>
+           <div className={`chat-bubble text-white bg-blue-500 ${bubbleBgColor} pb-2 `}>{props.msg.message}</div>
+           <div className={'chat-footer opacity-50 text-xs flex gap-1 items-center text-white'}> {formattedTime}</div>
 
           
 		
--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,19 +1,31 @@
 import Message from "./Message";
 import useGetMessages from "../../hooks/useGetMessages";
 import MessageSkeleton from "../skeletons/MessageSkeleton";
+import { useRef,useEffect } from "react";
 
 const Messages = () => {
   const { loading, messages } = useGetMessages();
+  const lastMessageRef = useRef();
 
-  console.log("messages:--", messages);
+  useEffect(() => {
+    setTimeout(() => {
+      lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
+    }, 100);
+  }, [messages]);
+
   return (
     <div className="px-4 flex-1 overflow-auto">
-		{!loading && messages.map((message) => <Message key={message._id} msg={message.message} />)}
-	
+      {!loading &&
+        messages.map((message) => (
+          <div key={message._id} ref={lastMessageRef}>
+            <Message msg={message} />
+          </div>
+        ))}
+
       {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
-	  {!loading && messages.length === 0 && (
-				<p className='text-center'>Send a message to start the conversation</p>
-			)}
+      {!loading && messages.length === 0 && (
+        <p className="text-center">Send a message to start the conversation</p>
+      )}
     </div>
   );
 };
